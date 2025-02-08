7c635c60b83d45adcf1e04bd5f543c58dbd62100
rashednsu172@gmail.com <Rashedul islam>
2025-02-08 21:46:30 +0600

    lost-item -show

--- src/components/modules/Home/RecentPost.tsx
+++ src/components/modules/Home/RecentPost.tsx
@@ -3,7 +3,15 @@ import Link from "next/link";
 
 import Container from "../../ui/Container";
 
+import { getRecentPost } from "@/src/services/RecentPost";
+import Card from "../../ui/Card";
+import { IPost } from "@/src/types";
+
 export default async function RecentPosts() {
+  const { data: post } = await getRecentPost();
+
+  console.log(post);
+
   return (
     <Container>
       <div className="section-title my-8">
@@ -13,7 +21,12 @@ export default async function RecentPosts() {
         </p>
       </div>
       <div className="my-8 grid justify-center gap-10 sm:grid-cols-1 md:grid-cols-4">
-        <h1>Recent post 1</h1>
+        {/* {post.map((item: any) => (
+          <p key={item._id}>{item.title}</p>
+        ))} */}
+        {post.map((item: IPost) => (
+          <Card key={item._id} post={item} />
+        ))}
       </div>
       <div className="flex justify-center">
         <Button className="rounded-md bg-default-900 text-default" size="md">
